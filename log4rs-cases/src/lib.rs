222486a38604d510dfc821304b81d937d5a46ae1
x7c1 <jmtaro+x7c1@gmail.com>
2019-08-10 12:38:51 +0900

    add usage sample

--- log4rs-cases/src/lib.rs
+++ log4rs-cases/src/lib.rs
@@ -1,3 +1,10 @@
+#[macro_use]
+extern crate failure;
+
+mod error;
+pub mod rotate;
+pub use crate::error::Result as CasesResult;
+
 pub fn hello(name: &str) -> String {
     format!("hello, {}!", name)
 }
@@ -6,6 +13,6 @@ pub fn hello(name: &str) -> String {
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(hello("world"), "hello, world!");
+        assert_eq!(crate::hello("world"), "hello, world!");
     }
 }
--- sample-app/src/main.rs
+++ sample-app/src/main.rs
@@ -1,6 +1,30 @@
+#[macro_use]
+extern crate log;
+
 use log4rs_cases::hello;
+use log4rs_cases::CasesResult;
+use log4rs_cases::rotate::Setting;
+use log4rs::config::Config;
 
 fn main() {
+    let config = setup().unwrap();
+    log4rs::init_config(config).unwrap();
+
     let greeting = hello("world");
     println!("{}", greeting);
+    info!("info: {}", greeting);
+}
+
+pub fn setup() -> CasesResult<Config> {
+    let log_dir = "./sample";
+    let setting = Setting {
+        appender_name: "sample_log_appender".to_string(),
+        file_pattern: format!("{}/{}", log_dir, "sample.{}.log"),
+        file_path: format!("{}/{}", log_dir, "sample.log"),
+        log_level: "info".to_string(),
+        limit_size_kb: 1000,
+        limit_file_count: 3,
+    };
+    let config = setting.configure()?;
+    Ok(config)
 }
